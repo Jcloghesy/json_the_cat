1405aa124d2cb747843321dd238dc695abdc9859
John <john.cloghesy@gmail.com>
2023-04-29 20:03:42 -0600

    SETUP: Create initial code file & require request

---
+++ breed.js
@@ -0,0 +1,7 @@
+/**  ******** Main Project File to Implement    ******** 
+ *  This is a command line application that allows users to enter the name of 
+ *  a dog breed and receive a short description of that breed.  The application 
+ *  queries the 'thecatapi' via its JSON api.  
+ */ 
+
+const request = require("request");

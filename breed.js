8ae84d2264d536b8cc05e6b7a3f7c390d687b0a9
John <john.cloghesy@gmail.com>
2023-04-29 20:10:43 -0600

    Implement function using node

--- breed.js
+++ breed.js
@@ -4,4 +4,49 @@
  *  queries the 'thecatapi' via its JSON api.  
  */ 
 
-const request = require("request");
+const fetchBreedDescription = function (breedName, callback) {
+  const request = require("request");
+  const apiEndPoint = "https://api.thecatapi.com/v1/breeds/search?q=";
+
+  request(apiEndPoint + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, `We have an error accessing the API: ${apiEndPoint}`);
+    }
+
+    if (response.statusCode === 200) {
+      const data = JSON.parse(body);
+      if (data.length > 0) {
+        const searchResult = `You searched for ${breedName}\n${constrainText(
+          data[0].description,
+          60
+        )}`;
+        callback(null, data[0].description);
+      } else {
+        callback(
+          "Not Found",
+          `We didn't find any feline breeds that matched your search of " ${breedName}`
+        );
+        return;
+      }
+    } else {
+      callback(
+        response.statusCode,
+        `ERROR FETCHING DATA: ${response.statusCode}`
+      );
+    }
+  });
+};
+
+const constrainText = function (inputText, textLength) {
+  let outputString = "";
+  for (let i = 0; i <= inputText.length; i++) {
+    if (i % textLength === 0) {
+      outputString += "\n";
+    }
+    outputString += inputText.charAt(i);
+  }
+  return outputString;
+};
+
+module.exports = { fetchBreedDescription };
+
